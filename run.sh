3f28cf7fc59c41be668f0e469d728e06190e9ba2
beamer2k23 <132817446+beamer2k23@users.noreply.github.com>
2024-05-26 13:23:37 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@
 if [ $# -gt 0 ]; then
   # append relay config if any parameters are given
   streamripper "$@" -r 8000 -R 0
+  echo "streamripper started..."
 else
   # otherwise, default to no args (=> help output)
   streamripper
