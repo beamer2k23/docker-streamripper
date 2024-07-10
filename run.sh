c2161e63ac98c4f8e952bb6c0ffb7595e7ffb08d
beamer2k23 <132817446+beamer2k23@users.noreply.github.com>
2024-07-10 21:32:51 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ cron && tail -f /var/log/cron.log
 
 if [ $# -gt 0 ]; then
   # append relay config if any parameters are given
-  streamripper "$@" -r 8000 -R 0
+  streamripper "$@" -d /home/streamripper/destination -D "%a - %T - %A" --quiet -o larger --xs_silence_length=1000 --xs_search_window=6000:6000 --xs_offset=0 --xs_padding=500:0 --codeset-filesys=UTF-8 --codeset-id3=ISO-8859-1 --codeset-metadata=ISO-8859-1 --codeset-relay=ISO-8859-1 -r 8000 -R 0
   echo "streamripper started..."
 else
   # otherwise, default to no args (=> help output)
