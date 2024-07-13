65213b42777c3cc0267440c9fd1c3b2351c75b35
beamer2k23 <132817446+beamer2k23@users.noreply.github.com>
2024-07-13 22:08:13 +0200

    Update cleanstreamripper.sh

--- cleanstreamripper.sh
+++ cleanstreamripper.sh
@@ -2,28 +2,31 @@
 
 cd /home/streamripper/destination
 
-ls -A1 ./*.mp3 | while read SOURCETRACKFILENAME; do
+if [ -f "zyx.txt" ]; then
 
-	ALBUM="${SOURCETRACKFILENAME#*- }"
-	ALBUM="${ALBUM%% - *mp3}"
-	#ALBUM="(?<=^- )\S.*?(?= - )"
-	echo "${ALBUM}" >> "LISTING.txt"	# mit Windows-Newline \n für Vergleichbarkeit mit ZYX.txt
+	ls -A1 ./*.mp3 | while read SOURCETRACKFILENAME; do
 	
-done
+		ALBUM="${SOURCETRACKFILENAME#*- }"
+		ALBUM="${ALBUM%% - *mp3}"
+		#ALBUM="(?<=^- )\S.*?(?= - )"
+		echo "${ALBUM}" >> "LISTING.txt"	# mit Windows-Newline \n für Vergleichbarkeit mit ZYX.txt
+		
+	done
+	
+	grep -Fx -f "LISTING.txt" "zyx.txt" > "LISTING_REMOVE.txt"
+	
+	
+	while read ALBUM
+	do
+		#ALBUM=${ALBUM//[$'\t\r\n']}	# ZEILENUMBRUECHE ENTFERNEN
+		rm -f \ -\ "${ALBUM}"\ -\ *.mp3
+	done < "LISTING_REMOVE.txt"
+	#done < "../ZYX.txt"
+	
+	rm "LISTING_REMOVE.txt"
+	rm "LISTING.txt"
 
-grep -Fx -f "LISTING.txt" "zyx.txt" > "LISTING_REMOVE.txt"
-
-
-while read ALBUM
-do
-	ALBUM=${ALBUM//[$'\t\r\n']}	# ZEILENUMBRUECHE ENTFERNEN
-	rm -f \ -\ "${ALBUM}"\ -\ *.mp3
-done < "LISTING_REMOVE.txt"
-#done < "../ZYX.txt"
-
-rm "LISTING_REMOVE.txt"
-rm "LISTING.txt"
-
+fi
 
 # delete older incomplete files
 cd /home/streamripper/destination/incomplete
