0922265be78ee146c205e4da815e395a9a87702c
beamer2k23 <132817446+beamer2k23@users.noreply.github.com>
2024-07-16 21:40:09 +0200

    Update cleanstreamripper.sh

--- cleanstreamripper.sh
+++ cleanstreamripper.sh
@@ -19,7 +19,7 @@ if [ -f "zyx.txt" ]; then
 	while read ALBUM
 	do
 		ALBUM=${ALBUM//[$'\t\r\n']}	# ZEILENUMBRUECHE ENTFERNEN
-		rm -f \ -\ ${ALBUM}\ -\ *.mp3
+		rm -f \ -\ "${ALBUM}"\ -\ *.mp3
 	done < "LISTING_REMOVE.txt"
 	#done < "../ZYX.txt"
 	
