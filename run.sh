85b087aeea81f9474cb48013f3775ebd68ea93b5
beamer2k23 <132817446+beamer2k23@users.noreply.github.com>
2024-07-19 22:43:13 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 #if [ $# -gt 0 ]; then
 # append relay config if any parameters are given
-streamripper $STREAMRIPPERSERVER -d /home/streamripper/destination -D "%a - %T - %A" --quiet -o larger --xs_silence_length=1000 --xs_search_window=6000:6000 --xs_offset=0 --xs_padding=500:0 --codeset-filesys=UTF-8 --codeset-id3=ISO-8859-1 --codeset-metadata=ISO-8859-1 --codeset-relay=ISO-8859-1 -r 8000 -R 0
+streamripper $STREAMRIPPERSERVER -d /home/streamripper/destination -D "%a - %T - %A" --quiet -o larger --xs_silence_length=1000 --xs_search_window=6000:6000 --xs_offset=0 --xs_padding=500:0 --codeset-filesys=UTF-8 --codeset-id3=ISO-8859-1 --codeset-metadata=ISO-8859-1 --codeset-relay=ISO-8859-1 -r 8000 -R 0 &
 echo "streamripper started..."
 #fi
 
